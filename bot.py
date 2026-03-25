624dc5fa17623fc81fbd72c17d3035157513c6e1
abatovaziz227-sudo <abatovaziz227@gmail.com>
2026-03-25 16:11:05 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,61 +1,39 @@
-import logging
-import yt_dlp
 import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import yt_dlp
 
-# logging
-logging.basicConfig(level=logging.INFO)
+BOT_TOKEN = os.getenv("8793753588:AAHl8bYf6jLt8GiTlP3gBL_xTmIDRuUHU4c")
 
-# TOKEN (BU YERGA O'Z TOKENINGNI QO'Y)
-TOKEN = "8793753588:AAHl8bYf6jLt8GiTlP3gBL_xTmIDRuUHU4c"
-
-# /start komandasi
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("📥 Instagram video link yubor!")
+    await update.message.reply_text("Bot ishlayapti ✅ Link yubor!")
 
-# video yuklash funksiyasi
 async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
     url = update.message.text
 
-    msg = await update.message.reply_text("⏳ Yuklanmoqda...")
+    await update.message.reply_text("⏳ Yuklanmoqda...")
+
+    ydl_opts = {
+        'format': 'best[filesize<50M]',  # 50MB dan kichik video
+        'outtmpl': 'video.%(ext)s',
+    }
 
     try:
-        ydl_opts = {
-            'outtmpl': 'video.%(ext)s',
-            'format': 'mp4',
-            'quiet': True
-        }
-
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
             filename = ydl.prepare_filename(info)
 
-        # video yuborish
         with open(filename, 'rb') as video:
             await update.message.reply_video(video)
 
-        await msg.delete()
-
-        # faylni o‘chirish (joy tozalanadi)
-        if os.path.exists(filename):
-            os.remove(filename)
+        os.remove(filename)
 
     except Exception as e:
-        if "Timed out" in str(e):
-            pass
-        else:
-            await update.message.reply_text(f"❌ Xato: {e}")
+        await update.message.reply_text("❌ Xatolik yoki video juda katta!")
 
-# botni ishga tushirish
-def main():
-    app = ApplicationBuilder().token(TOKEN).build()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, download))
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("download", download))
 
-    print("✅ Bot ishlayapti...")
-    app.run_polling()
-
-if __name__ == "__main__":
-    main()
+app.run_polling()
