0554e6220a866e723af36aa6dae39890ed2726f4
abatovaziz227-sudo <abatovaziz227@gmail.com>
2026-03-25 19:54:32 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,16 +7,31 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 # logging
 logging.basicConfig(level=logging.INFO)
 
-# TOKEN
 TOKEN = "8793753588:AAHl8bYf6jLt8GiTlP3gBL_xTmIDRuUHU4c"
+ADMIN_ID = 957915860  # <-- BU YERGA O'Z TELEGRAM IDINGNI QO'Y
 
-# /start komandasi
+# /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+
+    # Faqat senga yuboradi
+    await context.bot.send_message(
+        chat_id=ADMIN_ID,
+        text=f"🆕 Yangi foydalanuvchi:\n👤 {user.full_name}\n🆔 {user.id}"
+    )
+
     await update.message.reply_text("📥 Instagram video link yubor!")
 
-# video yuklash funksiyasi
+# video yuklash
 async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
     url = update.message.text
+
+    # ADMIN ga xabar
+    await context.bot.send_message(
+        chat_id=ADMIN_ID,
+        text=f"📩 Link keldi:\n👤 {user.full_name}\n🔗 {url}"
+    )
 
     msg = await update.message.reply_text("⏳ Yuklanmoqda...")
 
@@ -25,27 +40,25 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
             'outtmpl': 'video.%(ext)s',
             'format': 'mp4',
             'quiet': True,
-            'cookiefile': 'cookies.txt'   # 👈 MUHIM QISM
+            'cookiefile': 'cookies.txt'   # 🍪 MUHIM
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
             filename = ydl.prepare_filename(info)
 
-        # video yuborish
         with open(filename, 'rb') as video:
             await update.message.reply_video(video)
 
         await msg.delete()
 
-        # faylni o‘chirish
         if os.path.exists(filename):
             os.remove(filename)
 
     except Exception as e:
         await update.message.reply_text(f"❌ Xato: {e}")
 
-# botni ishga tushirish
+# run
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
